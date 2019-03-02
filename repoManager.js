1b5041dd98bf502cf57e612c9259071e17290a19
Robert Pooley <robertcpooley@gmail.com>
2019-03-01 20:08:08 -0500

    Added ability to tag repositories

--- repoManager.js
+++ repoManager.js
@@ -5,24 +5,41 @@ const exec = require('child_process').exec;
 class RepoManager {
 
     /**
-     * @param root (String) path to directory containing all the repos
+     * @param {string} root path to directory containing all the repos
+     * @param {string} repoUrl The prefix of all repository URLs
      */
     constructor(root, repoUrl) {
         this.root = root;
         if (!repoUrl.endsWith('/')) repoUrl += '/';
         this.repoUrl = repoUrl;
+        this.tagFile = path.join(__dirname, 'tags.json');
+
+        if (!fs.existsSync(this.root)) {
+            console.log(`Root directory "${this.root}" does not exist, creating it now.`);
+            fs.mkdirSync(this.root);
+        }
+
+        if (!fs.existsSync(this.tagFile)) {
+            console.log(`Tags file "${this.tagFile}" does not exist, creating it now.`);
+            fs.writeFileSync(this.tagFile, '{}');
+        }
     }
 
     /**
      * @returns Array of {path, url}
      */
     getRepos() {
+        const tags = this.getTags();
         return fs.readdirSync(this.root)
             .map(name => {
-                return {
+                const repo = {
                     path: path.join(this.root, name),
                     url: this.repoUrl + name
                 };
+                if (name in tags) {
+                    repo.tag = tags[name];
+                }
+                return repo;
             })
             .filter(repo => {
                 const stat = fs.lstatSync(repo.path);
@@ -30,6 +47,12 @@ class RepoManager {
             });
     }
 
+    /**
+     * Create a repository with the given name in the root directory
+     *
+     * @param {string} name The name of the repository
+     * @returns {Promise} Promise with the result of the git init command
+     */
     createRepo(name) {
         return new Promise((resolve, reject) => {
             const repoPath = path.join(this.root, name);
@@ -52,6 +75,31 @@ class RepoManager {
                 }
             });
         });
+    }
+
+    /**
+     * @returns {object} Mapping from repo to tag
+     */
+    getTags() {
+        return JSON.parse(fs.readFileSync(this.tagFile, 'utf-8'));
+    }
+
+    /**
+     * Set a repo's tag
+     *
+     * @param {string} repo The repo
+     * @param {string} tag The new tag for the repo, or null
+     */
+    setTag(repo, tag) {
+        const tags = this.getTags();
+
+        if (tag) {
+            tags[repo] = tag;
+        } else {
+            delete tags[repo];
+        }
+
+        fs.writeFileSync(this.tagFile, JSON.stringify(tags, null, 2));
     }
 }
 
--- web.js
+++ web.js
@@ -23,7 +23,7 @@ class Web {
     initRSA() {
         const privateKeyPath = path.join(__dirname, 'private.key');
         const publicKeyPath = path.join(__dirname, 'public.key');
-        if (!fs.existsSync(privateKeyPath)) {
+        if (!fs.existsSync(privateKeyPath) || !fs.existsSync(publicKeyPath)) {
             const key = new NodeRSA({b: 512});
             fs.writeFileSync(privateKeyPath, key.exportKey('pkcs8-private-pem'));
             fs.writeFileSync(publicKeyPath, key.exportKey('pkcs8-public-pem'));
@@ -42,21 +42,27 @@ class Web {
         this.app.use(bodyParser.json());
     }
 
-    route(path, cb) {
+    route(path, args, cb) {
         this.app.post(path, (req, res) => {
             if (req.body) {
+                for (let i = 0; i < args.length; i++) {
+                    if (!(args[i] in req.body)) {
+                        res.json({error: `Missing argument: ${args[i]}`});
+                        return;
+                    }
+                }
                 cb(req, res);
             } else {
-                res.json({err: 'No body in request'});
+                res.json({error: 'No body in request'});
             }
         });
     }
 
-    authRoute(path, cb) {
-        this.route(path, (req, res) => {
+    authRoute(path, args, cb) {
+        this.route(path, ['token', ...args], (req, res) => {
             jwt.verify(req.body.token, this.publicKey, (err, decoded) => {
                 if (err || !decoded.auth) {
-                    res.json({err: 'Invalid token'});
+                    res.json({error: 'Invalid token'});
                 } else {
                     cb(req, res);
                 }
@@ -65,7 +71,7 @@ class Web {
     }
 
     routes() {
-        this.route('/auth', (req, res) => {
+        this.route('/auth', ['password'], (req, res) => {
             if (req.body.password === this.password) {
                 const token = jwt.sign({auth: true}, this.privateKey, {algorithm: 'RS256'});
                 res.json({
@@ -77,11 +83,11 @@ class Web {
             }
         });
 
-        this.authRoute('/repos', (req, res) => {
+        this.authRoute('/repos', [], (req, res) => {
             res.json({repos: this.manager.getRepos()});
         });
 
-        this.authRoute('/addrepo', (req, res) => {
+        this.authRoute('/addrepo', ['name'], (req, res) => {
             this.manager.createRepo(req.body.name)
                 .then(() => {
                     res.json({success: true});
@@ -92,6 +98,11 @@ class Web {
                         error: err
                     });
                 });
+        });
+
+        this.authRoute('/settag', ['repo', 'tag'], (req, res) => {
+            this.manager.setTag(req.body.repo, req.body.tag);
+            res.json({success: true});
         });
 
         this.app.use(express.static(path.join(__dirname, 'public')));
